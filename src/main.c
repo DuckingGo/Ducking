212af05b3c54a97808578982a1c6da32e10d2b09
Duckky <kuliah.informatika01@gmail.com>
2025-07-20 10:06:05 +0000

    learning while loop and do while loop

--- src/main.c
+++ src/main.c
@@ -11,12 +11,16 @@ struct Book
 
 void learning_struct_book(struct Book b);
 int learning_switch_statement(char grade);
+int learning_while_loop(int n);
+int learning_do_while_looop(int n);
 
 int main()
 {
     learning_switch_statement('A');
     struct Book b = {"Avatar", "Tim handrick", "Fantasy", 2008};
     learning_struct_book(b);
+    learning_while_loop(10);
+    learning_do_while_looop(100);
     return 0;
 }
 
@@ -49,4 +53,24 @@ int learning_switch_statement(char grade)
 void learning_struct_book(struct Book b)
 {
     printf("Book: %s by %s (%d)\n", b.title, b.author, b.year_release);
+}
+
+int learning_while_loop(int n)
+{
+    int i = 1;
+    while (i <= n)
+    {
+        printf("While Loop: %d\n", i);
+        i++;
+    }
+}
+
+int learning_do_while_looop(int n)
+{
+    int i = 1;
+    do {
+        printf("Do While Loop: %d\n", i);
+        i++;
+    } 
+    while (i <= n);
 }
